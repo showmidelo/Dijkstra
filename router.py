051d9243983354b20e6fe9667a9850e58f2313ab
showmidelo <showmidelo@yahoo.com>
2021-08-25 11:03:43 +0430

    reading vertex and edges

--- router.py
+++ router.py
@@ -9,3 +9,18 @@ class Router:
         # open the file and reading lines of it
         with open(map_file_address, 'r') as MAP:
             n, m = [int(i) for i in MAP.readline().split()]
+        # reading vertex
+            for i in range(n):
+                identity, y, x = (float(i) for i in MAP.readline().split())
+                identity = int(identity)
+                vertex = Vertex(identity, y, x)
+                self.__vertices.append(vertex)
+                v[identity] = vertex
+        # reading edges and add adjacent_vertices
+            for i in range(m):
+                id1, id2 = (int(i) for i in MAP.readline().split())
+                edge = Edge(v[id1], v[id2])
+                self.__edges[id1, id2] = edge
+                self.__edges[id2, id1] = edge
+                v[id1].adjacent_vertices.append(v[id2])
+                v[id2].adjacent_vertices.append(v[id1])
