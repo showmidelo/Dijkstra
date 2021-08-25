da232aea3e54262e1c7b40e6e5ef8b9212fc2b4a
showmidelo <showmidelo@yahoo.com>
2021-08-25 11:42:44 +0430

    Vertex Class

--- vertex.py
+++ vertex.py
@@ -1,5 +1,4 @@
 class Vertex:
-
     def __init__(self, identity, y, x, value=float('inf')):
         self.identity = identity
         self.y = y
@@ -7,7 +6,7 @@ class Vertex:
         self.adjacent_vertices = []
         self.__value = value
         self.perv = None
-        
+
     def get_id(self):
         return self.identity
 
